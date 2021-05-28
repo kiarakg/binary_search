ee25721ffb7861a2526e8f976999e84866f8e5b6
Kiara Guzman <kiara.kg11@gmail.com>
2021-05-28 14:17:29 -0400

    binary search defined

--- binary_search.py
+++ binary_search.py
@@ -11,3 +11,13 @@ def naive_search(l, target):
 
 
 def binary_search(l, target, low = None, high = None):
+    if low is None:
+        low = 0
+    if high is None:
+        high = len(l) -1
+    
+    if high < low:
+        return -1
+
+    # example l = [1, 3, 5, 10, 12]     should return 3
+    midpoint = (low + high) // 2    # 2
